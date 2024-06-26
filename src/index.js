98d4e7c9fe57067c6ed5b3c84222c82b6da3397c
LizzieKeegan <lizziekeegan16@gmail.com>
2024-06-26 13:18:56 +0700

    Resolved central alignment of weather-forecast

--- src/index.js
+++ src/index.js
@@ -61,13 +61,16 @@ function displayForecast() {
                 alt=""
                 width="38"
               />
-              <span class="weather-forecast-temperature-max">18</span>
+             <div class="temperature-container"> 
+             <span class="weather-forecast-temperature-max">18</span>
               <span class="weather-forecast-temperature-min">12</span>
+              </div>
             </div>
           </div>`;
   });
   forecast.innerHTML = forecastHtml;
 }
+
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
